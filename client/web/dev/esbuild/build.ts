d993318918e431d74878338147a4aa7124e74ec0
Quinn Slack <quinn@slack.org>
2023-10-01 22:17:55 -0700

    WIP client/web webpack -> esbuild

--- client/web/dev/esbuild/build.ts
+++ client/web/dev/esbuild/build.ts
@@ -14,7 +14,6 @@ import {
     buildMonaco,
     buildTimerPlugin,
 } from '@sourcegraph/build-config'
-import { isDefined } from '@sourcegraph/common'
 
 import { ENVIRONMENT_CONFIG, IS_DEVELOPMENT, IS_PRODUCTION } from '../utils'
 
@@ -70,7 +69,7 @@ export const BUILD_OPTIONS: esbuild.BuildOptions = {
         }),
         omitSlowDeps ? null : monacoPlugin(MONACO_LANGUAGES_AND_FEATURES),
         buildTimerPlugin,
-    ].filter(isDefined),
+    ].filter((plugin): plugin is esbuild.Plugin => plugin !== null),
     define: {
         ...Object.fromEntries(
             Object.entries({ ...ENVIRONMENT_CONFIG, SOURCEGRAPH_API_URL: undefined }).map(([key, value]) => [
@@ -92,11 +91,12 @@ export const BUILD_OPTIONS: esbuild.BuildOptions = {
 
 export const build = async (): Promise<void> => {
     const metafile = process.env.ESBUILD_METAFILE
-    const result = await esbuild.build({
+    const options: esbuild.BuildOptions = {
         ...BUILD_OPTIONS,
         outdir: STATIC_ASSETS_PATH,
         metafile: Boolean(metafile),
-    })
+    }
+    const result = await esbuild.build(options)
     if (metafile) {
         writeFileSync(metafile, JSON.stringify(result.metafile), 'utf-8')
     }
@@ -104,6 +104,12 @@ export const build = async (): Promise<void> => {
         const ctx = await buildMonaco(STATIC_ASSETS_PATH)
         await ctx.rebuild()
         await ctx.dispose()
+    }
+
+    if (process.env.WATCH) {
+        const ctx = await esbuild.context(options)
+        await ctx.watch()
+        await new Promise(() => {}) // wait forever
     }
 }
 
